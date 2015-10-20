dac14863c2a9b150ba3cf5f3039fb6f6f664d1dd
gmadhumati <gmadhumati@gmail.com>
2015-10-20 15:18:59 -0700

    Updated the script

--- PerlDemo/form.pl
+++ PerlDemo/form.pl
@@ -13,12 +13,10 @@
 	use LWP::Simple;
 	use MIME::Base64;
 	use GD::Graph::bars;
-
+	use File::Fetch;
 	$ua = LWP::UserAgent->new;
-	 
-	my %dispatch = (
-		'/sequence' => \&process_html,
-	);
+
+	my %dispatch = ( '/sequence' => \&process_html, );
 
 	sub handle_request {
 		my $self = shift;
@@ -41,7 +39,7 @@
 		}
 
 	}
-	
+
 	sub process_html {
 
 		my $cgi = shift;    # CGI.pm object
@@ -111,7 +109,7 @@
                 ',
 			},
 		);
-		print $cgi->h2("Sequence Processing");
+		print $cgi->h2("Histogram Plot of GC content of the top BLAST 10 hits of the input sequence");
 	}
 
 	# Outputs a footer line and end html tags
@@ -122,11 +120,16 @@
 
 	# Displays the results of the form
 	sub display_results {
-		my ($cgi) = @_;
+		my ($cgi)        = @_;
 		my $usersequence = $cgi->param('user_sequence');
-		my $encoded_str = blast_sequence($usersequence);
-		
-		print $cgi->img({-src=>"data:image/png;base64,$encoded_str",-align=>"center"});
+		my $encoded_str  = blast_sequence($usersequence);
+
+		print $cgi->img(
+			{
+				-src   => "data:image/png;base64,$encoded_str",
+				-align => "center"
+			}
+		);
 
 	}
 
@@ -165,7 +168,7 @@
 
 	#sequence blast
 	sub blast_sequence {
-		$ua = LWP::UserAgent->new;
+
 		$sequence      = shift;
 		$encoded_query = '';
 		$encoded_query = $encoded_query . uri_escape($sequence);
@@ -200,7 +203,8 @@
 		while (true) {
 			sleep 5;
 
-			$req = new HTTP::Request GET => "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=$rid";
+			$req = new HTTP::Request GET =>
+"http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=$rid";
 			$response = $ua->request($req);
 
 			if ( $response->content =~ /\s+Status=WAITING/m ) {
@@ -210,7 +214,8 @@
 			}
 
 			if ( $response->content =~ /\s+Status=FAILED/m ) {
-				print STDERR "Search $rid failed; please report to blast-help\@ncbi.nlm.nih.gov.\n";
+				print STDERR
+"Search $rid failed; please report to blast-help\@ncbi.nlm.nih.gov.\n";
 				exit 4;
 			}
 
@@ -236,29 +241,32 @@
 		}    # end poll loop
 
 		# retrieve and display results
-		$req = new HTTP::Request GET => "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&FORMAT_TYPE=Text&RID=25A6RM6D014";
+		$req = new HTTP::Request GET =>
+"http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&FORMAT_TYPE=Text&RID=$rid";
 		$response = $ua->request($req);
 
 		@blast_output = split( "\n", $response->content );
 
 		my @tophits_ids = '';
 		my $i           = 0;
-		
+		my $c           = 0;
+		$len_tophits_ids = 0;
+
 		#retrieve ids of the top set of results
 		foreach $blast_output_line (@blast_output) {
 			chomp $blast_output_line;
+
 			if ( $blast_output_line =~ m/^\w+\|(.*)\.\d+\|.*\d+/ ) {
-				if ( $#tophits_ids < 10 ) {
-					push( @tophits_ids, $1 );
-				}
-				else {
+				push( @tophits_ids, $1 );
+				$len_tophits_ids = scalar @tophits_ids;
+				if ( $len_tophits_ids > 11 ) {
 					last;
 				}
-			}
 
+			}
 		}
 
-		 my $file = get_fasta(@tophits_ids);
+		my $file = get_fasta(@tophits_ids);
 
 		my %gc = get_gc($file);
 		$encoded_str = make_plot(%gc);
@@ -267,39 +275,24 @@
 	}
 
 	sub get_fasta {
+
+		my (@acc_array) = @_;
+
+		$acc_ids = join( "\,", @acc_array );
+		$acc_ids =~ s/^,//;
 		
-		my(@acc_array) = @_;
-		$file = "tmpfastaoutput.fa";
-		open( FASTA, ">$file" ) || die "cannot open the file - $!\n";
-
-		#append [accn] field to each accession
-		for ( $i = 0 ; $i < @acc_array ; $i++ ) {
-			$acc_array[$i] .= "[accn]";
-		}
-
-		#join the accessions with OR
-		$query = join( '+OR+', @acc_array );
-
-		#assemble the esearch URL
+		
+		my $encode_ids = '';
+		$encode_ids = $encode_ids . uri_escape($acc_ids);
+		
 		$base = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
-		$url  = $base . "esearch.fcgi?db=nucleotide&term=$query&usehistory=y";
+		$url  = $base
+		  . 'efetch.fcgi?db=nuccore&id='.$encode_ids.'&rettype=fasta&retmode=text';
 
-		#post the esearch URL
-		$output = get($url);
+		my $fastaout = "tmpfasta.fa";
+		getstore($url,$fastaout);
 
-		#parse WebEnv and QueryKey
-		$web = $1 if ( $output =~ /<WebEnv>(\S+)<\/WebEnv>/ );
-		$key = $1 if ( $output =~ /<QueryKey>(\d+)<\/QueryKey>/ );
-
-		#assemble the efetch URL
-		$url = $base . "efetch.fcgi?db=nucleotide&query_key=$key&WebEnv=$web";
-		$url .= "&rettype=fasta&retmode=text";
-
-		#post the efetch URL
-		$fasta = get($url);
-		print FASTA "$fasta\n";
-		
-		return $file;
+		return $fastaout;
 
 	}
 
@@ -319,16 +312,17 @@ sub get_gc {
 	}
 
 	while (<INFASTA>) {
-
+		chomp;
 		if ( $_ =~ m/^>/ ) {
 			@header = split( /\|/, $_ );
 			$id = "'" . $header[3] . "'";
 			my $seq = '';
 			$flag = 1;
+			
 
 		}
-		if ( $_ =~ m/([ATGCN]+)$/ ) {
-			$seq .= $1;
+		if ( $_ =~ m/^([ATGCN]+)$/ ) {
+			$seq .= $1;			
 			$flag = 0;
 		}
 
@@ -350,7 +344,6 @@ sub get_gc {
 			$seq_gc{$id} = $gccontent;	
 			$seq = '';
 		}
-
 	}
 	return %seq_gc;
 
@@ -360,8 +353,7 @@ sub get_gc {
 		my %gc = @_;
 
 		my @xvalues = [ keys %gc ];
-		my @y = [ values %gc ];
-
+		my @y       = [ values %gc ];
 
 		@data = (
 
@@ -379,29 +371,29 @@ sub get_gc {
 		$graph->set_x_label_font(GD::gdGiantFont);
 		$graph->set_y_label_font(GD::gdGiantFont);
 		$graph->set(
-			title        => "GC content",
-			x_label      => "Sequences",
-			y_label      => "Percent",
-			y_max_value  => 0,
-			y_max_value  => 100,
-			bgclr        => "white",
-			labelclr     => "black",
-			textclr      => "green",
-			axislabelclr => "red",
-			bar_spacing => 4,
+			title             => "GC content",
+			x_label           => "Sequences",
+			y_label           => "Percent",
+			y_max_value       => 0,
+			y_max_value       => 100,
+			bgclr             => "white",
+			labelclr          => "black",
+			textclr           => "green",
+			axislabelclr      => "red",
+			bar_spacing       => 5,
 			x_labels_vertical => 1,
 
 		) or die $graph->error;
 
 		my $gd = $graph->plot( \@data ) or die $graph->error;
-		
+
 		my $IMG = '';
-		$IMG = $gd->gif;
-		$encoded = encode_base64( $IMG );
-	
+		$IMG     = $gd->gif;
+		$encoded = encode_base64($IMG);
+
 		return $encoded;
 	}
-	
+
 }    # end of package MyWebServer
 
 # start the server on port 8080
